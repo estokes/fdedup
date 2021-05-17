5c777f85061144221cb78e0d1fc1b244a98b8892
Eric Stokes <letaris@gmail.com>
2021-05-17 16:56:21 -0400

    remove debug

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use md5::Digest;
 use parking_lot::Mutex;
 use std::{
     collections::HashMap,
+    mem,
     path::{Path, PathBuf},
     sync::Arc,
-    mem,
 };
 use tokio::{
     fs::{read_dir, File},
@@ -17,10 +17,9 @@ use tokio::{
 };
 
 async fn scan_file<P: AsRef<Path>>(permit: OwnedSemaphorePermit, path: P) -> Result<Digest> {
-    dbg!(());
     const MMAP_LEN: u64 = 32384; // mmap files bigger than 32k
     let res = {
-        let mut fd = dbg!(File::open(path).await?);
+        let mut fd = File::open(path).await?;
         let md = fd.metadata().await?;
         if md.len() <= MMAP_LEN {
             let mut contents = [0u8; 32384];
@@ -58,29 +57,24 @@ async fn scan_dir<P: AsRef<Path>>(
 ) -> Result<()> {
     let permit = dir_sem.acquire_owned().await?;
     let mut dirents = read_dir(path).await?;
-    while let Some(dirent) = dbg!(dirents.next_entry().await?) {
+    while let Some(dirent) = dirents.next_entry().await? {
         let ft = dirent.file_type().await?;
         if ft.is_symlink() {
             continue; // skip it
         } else if ft.is_dir() {
-            dbg!(());
             let path = dirent.path();
             dirs.lock().push(path);
-            dbg!(());
         } else {
             let res = res.clone();
             let permit = file_sem.clone().acquire_owned().await?;
-            let path = dbg!(dirent.path());
-            let task = task::spawn(async move {
-                dbg!(());
-                let digest = dbg!(scan_file(permit, &path).await?);
+            let path = dirent.path();
+            tasks.lock().push(task::spawn(async move {
+                let digest = scan_file(permit, &path).await?;
                 res.lock().entry(digest).or_insert_with(Vec::new).push(path);
                 Ok(())
-            });
-            tasks.lock().push(task);
+            }));
         }
     }
-    dbg!(());
     drop(permit);
     Ok(())
 }
@@ -104,10 +98,9 @@ async fn main() -> Result<()> {
             let res = res.clone();
             let file_sem = file_sem.clone();
             let dir_sem = dir_sem.clone();
-            let task = task::spawn(async move {
+            tasks.lock().push(task::spawn(async move {
                 Ok(scan_dir(tasks_, dirs, res, dir_sem, file_sem, dir).await?)
-            });
-            tasks.lock().push(task);
+            }));
         }
         for task in tasks_ {
             task.await??;
