2ae3400ae98d0a73b38067c8d5fd17e5dc06b85d
Eric Stokes <letaris@gmail.com>
2021-05-17 17:06:46 -0400

    only spawn tasks for large files

--- src/main.rs
+++ src/main.rs
@@ -16,32 +16,38 @@ use tokio::{
     task::{self, JoinHandle},
 };
 
+const MMAP_LEN: usize = 32384;
+
+// for files < MMAP_LEN
 async fn scan_file<P: AsRef<Path>>(permit: OwnedSemaphorePermit, path: P) -> Result<Digest> {
-    const MMAP_LEN: u64 = 32384; // mmap files bigger than 32k
     let res = {
         let mut fd = File::open(path).await?;
-        let md = fd.metadata().await?;
-        if md.len() <= MMAP_LEN {
-            let mut contents = [0u8; 32384];
-            let mut pos: usize = 0;
-            loop {
-                let n = fd.read(&mut contents[pos..]).await?;
-                if n > 0 {
-                    pos += n;
-                } else {
-                    break;
-                }
+        let mut contents = [0u8; MMAP_LEN];
+        let mut pos: usize = 0;
+        loop {
+            let n = fd.read(&mut contents[pos..]).await?;
+            if n > 0 {
+                pos += n;
+            } else {
+                break;
             }
-            Ok(md5::compute(&contents[0..pos]))
-        } else {
-            task::block_in_place(|| {
-                let fd = fd
-                    .try_into_std()
-                    .map_err(|_| anyhow::anyhow!("operation in progress"))?;
-                let mmap = unsafe { Mmap::map(&fd)? };
-                Ok(md5::compute(&mmap[..]))
-            })
         }
+        Ok(md5::compute(&contents[0..pos]))
+    };
+    drop(permit);
+    res
+}
+
+async fn scan_file_mmap<P: AsRef<Path>>(permit: OwnedSemaphorePermit, path: P) -> Result<Digest> {
+    let res = {
+        let fd = File::open(path)
+            .await?
+            .try_into_std()
+            .map_err(|_| anyhow::anyhow!("operation in progress"))?;
+        task::block_in_place(|| {
+            let mmap = unsafe { Mmap::map(&fd)? };
+            Ok(md5::compute(&mmap[..]))
+        })
     };
     drop(permit);
     res
@@ -58,18 +64,24 @@ async fn scan_dir<P: AsRef<Path>>(
     let permit = dir_sem.acquire_owned().await?;
     let mut dirents = read_dir(path).await?;
     while let Some(dirent) = dirents.next_entry().await? {
+        let md = dirent.metadata().await?;
         let ft = dirent.file_type().await?;
         if ft.is_symlink() {
             continue; // skip it
         } else if ft.is_dir() {
             let path = dirent.path();
             dirs.lock().push(path);
+        } else if md.len() <= MMAP_LEN as u64 {
+            let path = dirent.path();
+            let permit = file_sem.clone().acquire_owned().await?;
+            let digest = scan_file(permit, &path).await?;
+            res.lock().entry(digest).or_insert_with(Vec::new).push(path);
         } else {
             let res = res.clone();
             let permit = file_sem.clone().acquire_owned().await?;
             let path = dirent.path();
             tasks.lock().push(task::spawn(async move {
-                let digest = scan_file(permit, &path).await?;
+                let digest = scan_file_mmap(permit, &path).await?;
                 res.lock().entry(digest).or_insert_with(Vec::new).push(path);
                 Ok(())
             }));
